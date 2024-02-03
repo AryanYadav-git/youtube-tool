73e336c2ec628f20f1d8b054bbd994b8a583a97b
Aryan Yadav <yadavaryan466@gmail.com>
2024-02-03 23:06:37 +0530

    completed login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,51 @@
 import { Link, Divider, Button, Card, TextField, Typography } from '@mui/material'
 import GoogleIcon from '@mui/icons-material/Google';
+import { useState } from 'react';
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return <div style={{ display: 'flex', justifyContent: 'center', margin: '5rem 5rem', }}>
         <Card style={{ height: '30rem', width: '25rem' }}>
             <div style={{ margin: '2rem 2rem', display: 'flex', flexDirection: 'column', gap: '0.7rem' }}>
                 <center style={{ display: 'flex', flexDirection: 'column', gap: '1.7rem' }} >
                     <Typography variant='h6'>Login</Typography>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" style={{}} />
+                    <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) =>{
+                        let elemt = e.target;
+                        setEmail(elemt.value);
+                    }} />
                     <div style={{display: 'flex', flexDirection: 'column'}}>
-                        <TextField id="outlined-basic" label="Password" variant="outlined" />
+                        <TextField id="outlined-basic" label="Password" variant="outlined" onChange={(e) =>{
+                        let elemt = e.target;
+                        setPassword(elemt.value);
+                    }} />
                         <Link href="#" underline="always" style={{display:"flex", width:'100%', justifyContent:'end'}}>
                             Forgot Password
                         </Link>
                     </div>
-                    <Button variant='contained'>Login</Button>
+                    <Button variant='contained' onClick={() => {
+                        function callback2(data) {
+                            localStorage.setItem("token", data.token);
+                            window.location = "/"
+                        }
+
+                        function callback1(res) {
+                            res.json().then(callback2);
+                        }
+
+                        fetch("http://localhost:3000/admin/login/", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                username: email,
+                                password: password
+                            }),
+                            headers: {
+                                "Content-type": "application/json",
+                                "Authorization": "bearer " + localStorage.getItem("token")
+                            }
+                        }).then(callback1);
+                    }}>Login</Button>
                     <Divider>Or</Divider>
                     <Button variant='text' startIcon={<GoogleIcon/>}>Login with Google</Button>
                 </center>
